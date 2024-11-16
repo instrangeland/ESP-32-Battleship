6d66f1dc8e2ab378faf71661d1fd88b72dcf30bf
FlexyRexy <112587570+FlexyRexy@users.noreply.github.com>
2024-11-16 16:00:32 -0700

    Updated grid size in config.h

--- lab07/main/config.h
+++ lab07/main/config.h
@@ -4,9 +4,9 @@
 #define CONFIG_GAME_TIMER_PERIOD 40.0E-3f
 
 // Board
-#define CONFIG_BOARD_R 3 // Rows
-#define CONFIG_BOARD_C 3 // Columns
-#define CONFIG_BOARD_N 3 // Number of contiguous marks
+#define CONFIG_BOARD_R 10 // Rows
+#define CONFIG_BOARD_C 10 // Columns
+// #define CONFIG_BOARD_N 3 // Number of contiguous marks
 // #define CONFIG_BOARD_R 5 // Rows
 // #define CONFIG_BOARD_C 7 // Columns
 // #define CONFIG_BOARD_N 4 // Number of contiguous marks
@@ -16,7 +16,8 @@
 // Colors
 #define CONFIG_BACK_CLR rgb565(0, 16, 42)
 #define CONFIG_GRID_CLR WHITE
-#define CONFIG_MARK_CLR YELLOW
+#define CONFIG_MISS_MARK_CLR YELLOW
+#define CONFIG_HIT_MARK_CLR RED
 #define CONFIG_HIGH_CLR GREEN
 #define CONFIG_MESS_CLR CYAN
 
